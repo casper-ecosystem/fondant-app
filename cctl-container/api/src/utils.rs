f41b4496426313c17eef34222a596a67aecd6ea6
Dylan Ireland <dylan@casper.network>
2024-08-23 01:18:09 -0400

    Remove now unnecessary CORS headers

--- cctl-container/api/src/utils.rs
+++ cctl-container/api/src/utils.rs
@@ -127,29 +127,13 @@ http {{
             let proxy_pass = format!("proxy_pass http://localhost:{}/;", port);
 
             let full_location_block = format!("{}{{
-{}
-    proxy_set_header Host $host;
-    proxy_set_header X-Real-IP $remote_addr;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto $scheme;
-
-    # Add CORS headers
-    add_header 'Access-Control-Allow-Origin' '*' always;
-    add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, DELETE, OPTIONS' always;
-    add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range' always;
-
-    # Handle OPTIONS request
-    if ($request_method = 'OPTIONS') {{
-        add_header 'Access-Control-Allow-Origin' '*';
-        add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, DELETE, OPTIONS';
-        add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
-        add_header 'Access-Control-Max-Age' 1728000;
-        add_header 'Content-Type' 'text/plain; charset=utf-8';
-        add_header 'Content-Length' 0;
-        return 204;
-    }}
-}}
-", location_block, proxy_pass);
+                {}
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+            }}
+            ", location_block, proxy_pass);
 
             config.push_str(&full_location_block);
         }
